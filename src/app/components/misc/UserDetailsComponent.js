7e1f88a59ffa6af4d026c7fbdb515e9ac5cf0df3
PepperLola <julian.orion.wright@gmail.com>
2020-09-01 22:26:30 -0700

    changed font sizes, default channel icon, position of sign in button

--- src/app/components/misc/UserDetailsComponent.js
+++ src/app/components/misc/UserDetailsComponent.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
+import {stats} from "../../../index";
 
 export default class UserDetailsComponent extends Component {
     render() {
         let username = "The Morning Company";
+        let defaultIcon = "https://yt3.ggpht.com/a/AATXAJyVevgwtCWaOXufmNNMSYhJM15KSLuKh-hxmTGC=s800-c-k-c0xffffffff-no-rj-mo";
         return (
             <div className={"userDetails"}>
-                <img src={"https://eu.ui-avatars.com/api/?background=fff&color=000&size=128&name=The+Morning+Company&length=3&bold=true"} />
-                <h1>{username}</h1>
+                <img src={stats["profilePicture"] === undefined ? defaultIcon : stats["profilePicture"]} width={128}/>
+                <h1>{stats["username"] === undefined ? "Sign In!" : stats["username"]}</h1>
             </div>
         );
     }
