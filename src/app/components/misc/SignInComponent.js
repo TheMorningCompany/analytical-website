19a646ad5121cf5f290975128e018a31547f3f69
PepperLola <julian.orion.wright@gmail.com>
2020-09-06 23:29:43 -0700

    dark mode for sign in and out buttons:

--- src/app/components/misc/SignInComponent.js
+++ src/app/components/misc/SignInComponent.js
@@ -5,7 +5,7 @@ import {darkMode} from "../../../App";
 export default class SignInComponent extends Component {
     render() {
         return (
-            <div style={{background: darkMode ? "#fff" : "#000", color: darkMode ? "#000" : "#fff"}} className={"signInComponent"}>
+            <div className={"signInComponent"}>
                 <h1 onClick={oauthSignIn}>Sign In</h1>
             </div>
         );
