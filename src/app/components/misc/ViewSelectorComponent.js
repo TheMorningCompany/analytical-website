ffb7c94d1d67991925e89a17b80d2e9bfb6ec1e7
PepperLola <julian.orion.wright@gmail.com>
2021-01-27 12:45:50 -0800

    made view selector animated and fixed the selection, but there are a few things like the width that need to be fixed

--- src/app/components/misc/ViewSelectorComponent.js
+++ src/app/components/misc/ViewSelectorComponent.js
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export let selectedScreen = 0
 
 export const selectScreen = (screen) => {
-  selectedScreen = screen
-  const listEntries = document.getElementsByClassName('listEntry')
-  for (let i = 0; i < listEntries.length; i++) {
-    const element = listEntries[i]
-    element.className = element.className.replace(' selected', '')
+  selectedScreen = screen;
+  let entries = document.getElementsByClassName('listEntry');
+  for (let i in entries) {
+    let entry = entries[i];
+    entry.className = entry.className.replace(' selected', '')
+    if (i === selectedScreen) {
+      entry.className = entry.className + ' selected';
+    }
   }
-  document.getElementsByClassName(screen.toString())[0].className += ' selected'
+  console.log(document.getElementsByClassName('listEntry'));
 }
 
 export const ViewSelectorComponent = () => {
+  let [selectedScreen, setSelectedScreen] = useState(0);
   return (
     <div className={'viewSelectorContainer'}>
       <div className={'component viewSelectorComponent'}>
         <ul>
-          <Link className={'listEntry 0 selected'} to={{ pathname: '/' }}>
+          <Link onClick={() => setSelectedScreen(0)} className={'listEntry entry-0' + (selectedScreen === 0 ? ' selected' : '')} to={{ pathname: '/' }}>
             dashboard
           </Link>
-          <Link className={'listEntry 1'} to={{ pathname: '/videos' }}>
+          <Link onClick={() => setSelectedScreen(1)} className={'listEntry entry-1' + (selectedScreen === 1 ? ' selected' : '')} to={{ pathname: '/videos' }}>
             videos
           </Link>
-          <Link className={'listEntry 2'} to={{ pathname: '/playlists' }}>
+          <Link onClick={() => setSelectedScreen(2)} className={'listEntry entry-2' + (selectedScreen === 2 ? ' selected' : '')} to={{ pathname: '/playlists' }}>
             playlists
           </Link>
-          <Link className={'listEntry 3'} to={{ pathname: '/analytics' }}>
+          <Link onClick={() => setSelectedScreen(3)} className={'listEntry entry-3' + (selectedScreen === 3 ? ' selected' : '')} to={{ pathname: '/analytics' }}>
             analytics
           </Link>
-          <Link className={'listEntry 4'} to={{ pathname: '/commends' }}>
+          <Link onClick={() => setSelectedScreen(4)} className={'listEntry entry-4' + (selectedScreen === 4 ? ' selected' : '')} to={{ pathname: '/commends' }}>
             comments
           </Link>
-          <Link className={'listEntry 6'} to={{ pathname: '/monetization' }}>
+          <Link onClick={() => setSelectedScreen(5)} className={'listEntry entry-5' + (selectedScreen === 5 ? ' selected' : '')} to={{ pathname: '/monetization' }}>
             monetization
           </Link>
+          <div class="bgContainer">
+            <div class="bg"></div>
+          </div>
         </ul>
       </div>
     </div>
